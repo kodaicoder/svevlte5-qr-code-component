d2b717a6c7079c779397106743c541283b800a5b
nutchyleo <nutchapon.m@thaibev.com>
2025-03-10 19:57:56 +0700

    refactor: componentize a QR code card

---
+++ src/lib/component/QRCode.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	interface Props {
+		imageSource: string;
+		header: string;
+		description: string;
+	}
+	let { imageSource, header, description }: Props = $props();
+</script>
+
+<div class="card">
+	<div class="qrcode">
+		<img src={imageSource} alt="qr code" />
+	</div>
+	<div class="description">
+		<h1>{header}</h1>
+		<p>{description}</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<div class="card">
-	<div class="qrcode">
-		<img src="./images/image-qr-code.png" alt="qr code" />
-	</div>
-	<div class="description">
-		<h1>Improve your front-end skills by building projects</h1>
-		<p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-	</div>
-</div>
+<script>
+	import QrCode from '$lib/component/QRCode.svelte';
+</script>
+
+<QrCode
+	imageSource="./images/image-qr-code.png"
+	header="Improve your front-end skills by building projects"
+	description="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+/>
